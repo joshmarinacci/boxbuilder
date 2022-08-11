d7d75433fc4a40595686b1e7a38d7ff297005418
Josh Marinacci <joshua@marinacci>
2022-08-11 10:11:37 -0400

    make boxbuilder use the URL for storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import z from "zod";
+import React, {useEffect, useState} from 'react';
+import z, {ZodSchema} from "zod";
 import {cuboid} from "@jscad/modeling/src/primitives";
 // @ts-ignore
 import * as serializer from "@jscad/stl-serializer"
@@ -12,7 +12,8 @@ import {Geom3} from "@jscad/modeling/src/geometries/types";
 export const BoxSchema = z.object({
     width:z.number(),
     height:z.number(),
-    depth:z.number()
+    depth:z.number(),
+    thickness:z.number().min(1).max(5)
 })
 type Box = z.infer<typeof BoxSchema>;
 
@@ -23,28 +24,64 @@ function box_to_solids(new_box:Box):Geom3[] {
     ]
 }
 
-const start_box:Box = BoxSchema.parse({
+function make_start_doc(schema:ZodSchema, backup:object):Box {
+    const start_box:Box = schema.parse(backup)
+    try {
+        let q = window.location.search
+        // console.log("page start", q)
+        if(q && q.startsWith("?data=")) {
+            q = q.substring(6)
+            let json = decodeURIComponent(q)
+            let data = BoxSchema.parse(JSON.parse(json))
+            console.log("got data from url",data)
+            return data
+        }
+    }catch (e) {
+        console.log('error happened',e)
+    }
+    return start_box
+}
+
+const export_stl = (solids:Geom3[]) => {
+    const rawData = serializer.serialize({binary:true},solids)
+    const blob = new Blob(rawData,{type:"model/stl"})
+    forceDownloadBlob("sphere.stl",blob)
+}
+
+
+const default_box = {
     width:10,
     height:5,
     depth:5,
-})
+    thickness:2
+}
 
 function App() {
-    const [box, set_box] = useState<Box>(start_box)
-    const [solids, set_solids] = useState(()=>box_to_solids(box))
-    const export_stl = () => {
-        const rawData = serializer.serialize({binary:true},solids)
-        const blob = new Blob(rawData,{type:"model/stl"})
-        forceDownloadBlob("sphere.stl",blob)
-    }
+    const [box, set_box] = useState<Box>(()=> make_start_doc(BoxSchema, default_box))
+    const solids = box_to_solids(box)
+
     const update_box = (new_box:Box) => {
-        set_solids(box_to_solids(new_box))
         set_box(new_box)
+        let str = encodeURIComponent(JSON.stringify(new_box))
+        window.history.pushState(new_box,'box-state',"?data="+str)
     }
+    useEffect(()=>{
+        const history_changed = (event:PopStateEvent) => {
+            try {
+                set_box(BoxSchema.parse(event.state))
+            } catch (e) {
+                console.log("error restoring from history")
+            }
+        }
+        window.addEventListener("popstate",history_changed)
+        return () => {
+            window.removeEventListener("popstate",history_changed)
+        }
+    },[window])
     return <div>
         <AutoForm object={box} schema={BoxSchema} onChange={update_box}/>
         <Renderer solids={solids}  width={800} height={450}/>
-        <button onClick={export_stl}>to STL</button>
+        <button onClick={()=>export_stl(solids)}>to STL</button>
     </div>
 }
 
