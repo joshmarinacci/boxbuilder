358902aa12464ba932bac9b1293b986fe7387676
Josh Marinacci <276938+joshmarinacci@users.noreply.github.com>
2024-03-13 15:59:59 -0700

    add lid thickness and lid tolerance

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,19 @@ export const BoxSchema = z.object({
     width:z.number().min(1).int(),
     height:z.number().min(1).int(),
     depth:z.number().min(1).int(),
-    thickness:z.number().min(0.1).max(5),
+    wallThickness:z.number().min(0.1).max(5),
+    lidThickness: z.number().min(0.1).max(5),
+    lidTolerance: z.number().min(0.0).max(5),
     cornerRadius:z.number().min(1).max(5),
 })
 type Box = z.infer<typeof BoxSchema>;
 const default_box = {
     width:10,
-    height:5,
-    depth:5,
-    thickness:2,
+    height:10,
+    depth:10,
+    wallThickness:2,
+    lidThickness: 2,
+    lidTolerance: 2,
     cornerRadius:2,
 }
 
@@ -37,15 +41,18 @@ function min(numbers: number[]):number {
 }
 
 function box_to_solids(box:Box):Geometry[] {
+    // gap between the two shapes
     let gap = 3
-    let lid = 5
+    let lid = box.lidThickness
     // corner radius must be smaller than half the box dimensions
-    // let corner = min([box.width/2,box.depth/2,box.height/2,box.cornerRadius])
+    let corner = min([box.width/2,box.depth/2,box.height/2,box.cornerRadius])
     //thickness must be less than box dimensions
-    let thick = min([box.thickness, box.width-0.1])
+    let thick = min([box.wallThickness, box.width-0.1])
+    let lid_tol = box.lidTolerance
 
     try {
     return [
+        // the box
         transforms.translate(
      [-box.width/2-gap,0,box.height/2],
             subtract(
@@ -59,11 +66,12 @@ function box_to_solids(box:Box):Geometry[] {
                 })
             )
         ),
+        // the lid
         transforms.translate(
             [box.width/2+gap,0,lid/2],
             union(
                 cuboid({
-                    size:[box.width-thick, box.depth-thick, lid],
+                    size:[box.width-thick-lid_tol, box.depth-thick-lid_tol, lid],
                 }),
                 cuboid({
                     size:[box.width, box.depth, lid/2],
